cf97c03ebeddbaae3ab0520c53e12e3b9fd004a2
Tia Rose <shortyshort789@aol.com>
2019-05-20 16:13:13 -0700

    have all tests passing, have subtraction and multplication functions working

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -1,4 +1,36 @@
 'use strict';
 
+var faker = require('faker');
+
+var randomNumber = faker.random.number();
+console.log(randomNumber);
+
 const arithmetic = require('../lib/arithmetic.js');
+
+describe('Arithmatic module', () => {
+  it('requires only numbers - addition', () => {
+    let a = 'death';
+    let b = 'destruction';
+    expect (arithmetic.add(a,b)).toBeNull();
+  });
+
+  it('requires only numbers - subtraction', () => {
+    let a = 'depression';
+    let b = 'sadness';
+    expect (arithmetic.subtract(a,b)).toBeNull();
+  });
+
+  it('requires only numbers - multiply', () => {
+    let a = 'blackhole';
+    let b = 'emptiness';
+    expect (arithmetic.multiply(a,b)).toBeNull();
+  });
+
+  it('requires only numbers - division', () => {
+    let a = 'anxiety';
+    let b = 'loneliness';
+    expect (arithmetic.division(a,b)).toBeNull();
+  });
+});
+
 
