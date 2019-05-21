0af895793fd44078d2314f0951495a397e6bdfaf
Tia Rose <shortyshort789@aol.com>
2019-05-20 19:42:38 -0700

    working on greeting function

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const math = require('./lib/arithmetic.js');
 console.log(greet('JOHN'));
 console.log(math.add(1,3)); // 4
 console.log(math.subtract(1,3)); // -2
+console.log(math.multiply(5,3)); //15
+console.log(math.division(2,12)); //6
+
